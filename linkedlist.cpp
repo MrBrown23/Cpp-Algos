674249c0c60019272671be97d614d02d93b10c76
Gzuz <MRRbrown@protonmail.com>
2025-02-19 23:40:52 +0100

    Adding insertAfter function.

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -28,44 +28,73 @@ void insertAtFront(Node **head, int value)
     *head = temp;
 }
 
-void insertAtEnd(Node *head, int value)
+void insertAtEnd(Node **head, int value)
 {
-    Node* p = head;
+    Node* p = *head;
     Node *temp = new Node();
+    temp->value = value;
+    temp->next = NULL;
+    if(*head == NULL)
+    {
+        *head = temp;
+        return;
+    }
     while( p->next != NULL)
     {
         p = p->next;
     }
-    temp->value = value;
-    temp->next = NULL;
+    
     p->next = temp;
     
 }
 
+void insertAfter(Node **head, int value, int newvalue)
+{
+    Node *p = *head;
+    // Node *temp = new Node();
+    Node *temp = new Node();
+    temp->value = newvalue;
+    if(*head == NULL)
+    {
+        *head = temp;
+        temp->next = NULL;
+        return;
+    }
+    while(p->next != NULL)
+    {
+        if(p->value == value)
+        {
+            temp->next = p->next;
+            p->next = temp;
+            break;
+            
+
+        }
+        p = p->next;
+    }
+
+}
+
 int main()
 {
-    Node *head = new Node();
-    Node *second = new Node();
-    Node *third = new Node();
-
-    head->value = 11;
-    head->next = second;
-
-    second->value = 22;
-    second->next = third;
-
-    third->value = 33;
-    third->next = NULL;
+    Node *head = NULL;
+    
+    insertAtEnd(&head, 1);
+    insertAtEnd(&head, 2);
+    insertAtEnd(&head, 3);
 
     printlist(head);
 
     insertAtFront(&head, 77);
     printlist(head);
 
-    insertAtEnd(head,44);
+    insertAtEnd(&head,44);
     printlist(head);
 
-    insertAtEnd(head,55);
+    insertAtEnd(&head,55);
+    printlist(head);
+
+    insertAfter(&head, 3, 4);
     printlist(head);
     
     return 0;
