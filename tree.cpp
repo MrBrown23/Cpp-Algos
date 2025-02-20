0a3508045da3c674a20aea85ecb9c35c2ab5737e
Gzuz <MRRbrown@protonmail.com>
2025-02-21 00:16:00 +0100

    Adding print_dfs function.

--- tree.cpp
+++ tree.cpp
@@ -18,7 +18,33 @@ Node* createNode(int data)
 
 }
 
+void print_dfs(Node *root)
+{
+    if(root == nullptr)
+    {
+        cout << "<Empty>" << endl;
+        return;
+    }
+    
+    cout << "Value " << root->data << endl;
+    cout << "Left of " << root->data << endl;
+    print_dfs(root->left);
+    cout << "Right of " << root->data << endl;
+    print_dfs(root->right);
+}
+
 int main()
 {
+    Node* root = createNode(1);
+    Node *n1 = createNode(2);
+    Node *n2 = createNode(3);
+    Node *n3 = createNode(4);
 
+    root->left = n1;
+    root->right = n2;
+    n1->left = n3;
+
+    print_dfs(root);
+
+    return 0;
 }
