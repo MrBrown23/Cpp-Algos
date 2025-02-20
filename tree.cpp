7a8004193e7a4c55f843fb8ed6cf7ef11d4d14e8
Gzuz <MRRbrown@protonmail.com>
2025-02-21 00:04:15 +0100

    Creating tree.cpp and adding createNode function to it.

---
+++ tree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node* createNode(int data)
+{
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+    return newNode;
+
+}
+
+int main()
+{
+
+}
