2d36eb2c33f04d02a15d6d8b2d8ce62434307344
Gzuz <MRRbrown@protonmail.com>
2025-02-20 23:55:30 +0100

    Adding printQueue function.

--- queue.cpp
+++ queue.cpp
@@ -3,18 +3,24 @@
 
 using namespace std;
 
+void printqueue(queue<int> q)
+{
+    int num;
+    while(!q.empty())
+    {
+        num = q.front();
+        q.pop();  
+        cout << num << endl;
+    }
+}
+
 int main()
 {
     queue<int> numQueue;
     numQueue.push(1);
     numQueue.push(2);
     numQueue.push(3);
-    int num;
-    while(!numQueue.empty())
-    {
-        num = numQueue.front();
-        numQueue.pop();  
-        cout << num << endl;
-    }
+    printqueue(numQueue);
+    
     return 0;
 }
