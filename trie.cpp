d06ffe55c827c6297ace618c1792867e4f897ed8
Gzuz <MRRbrown@protonmail.com>
2025-02-21 23:52:09 +0100

    Adding inserttrie funtion.

--- trie.cpp
+++ trie.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
@@ -20,6 +21,35 @@ trienode *createnode()
     newnode->terminal = false;
 
     return newnode;
+
+}
+
+bool inserttrie(trienode **root, char *signedtext)
+{
+    if(*root == nullptr)
+    {
+        *root = createnode();
+    }
+
+    unsigned char *text = (unsigned char*)signedtext;
+
+    trienode *tmp = *root;
+    int length = strlen(signedtext);
+
+    for(int i=0; i < length; i++)
+    {
+        if(tmp->children[text[i]] == nullptr){
+            tmp->children[text[i]] = createnode();
+        }
+        tmp = tmp->children[text[i]];
+    }
+    if(tmp->terminal)
+    {
+        return false;
+    }
+    else{
+        return true;
+    }
 
 }
 
